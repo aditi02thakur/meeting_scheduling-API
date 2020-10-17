42f5f7dc348a3a075e4a5b0c01b32a16ecf74693
aditi02thakur <65253821+aditi02thakur@users.noreply.github.com>
2020-10-17 21:50:31 +0530

    added functions

--- rel.go
+++ rel.go
@@ -10,7 +10,7 @@ import (
 Meeting  struct{
 	ID int 'json:"id,omitempty"'
 	Title string 'json:"title"'
-	Participants string 'json:"participants"'
+	Participant string 'json:"participant"'
 	StartTime *Time 'json:"start_time"'
 	EndTime *Time 'json:"end_time"'
 	CreationTimestamp string 'json:"creationTimestamp"'
@@ -26,4 +26,24 @@ Occurrence struct {
 		StartTime *Time  'json:"start_time"'
 		EndTime *Time    'json:"end_time"'
 		RSVP string 'json:"rspv"'
+}
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/meetings", returnAllMeetings)
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+    handleRequests()
+}
+
+func returnAllMeetings(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Endpoint Hit: returnAllMeetings")
+    json.NewEncoder(w).Encode(Meetings)
 }
